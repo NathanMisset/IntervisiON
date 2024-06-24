d02e267601af4be060ce3000882f13fbb757a296
Nathan <nathan_misset@live.nl>
2024-06-24 16:31:34 +0200

    13/03/2024 Added a Kotlin String.kt to move all Strigs from String.xml to

---
+++ app/src/main/java/com/example/intervision/ui/String.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright Lectoraat Legal Management van de Hogeschool van Amsterdam
+ *
+ * Gemaakt door Nathan Misset 2024
+ */
+
+package com.example.intervision.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+
+import kotlin.String
+
+data class String(
+    // App
+    val nameApp: String = "Intervisie voor bezwaarbehandelaars",
+    val quitButtonApp: String = "Sluit",
+    val saveButtonApp: String = "Opgeslagen",
+    val backButtonApp: String = "Terug",
+    val nextButtonApp: String = "Volgende",
+    val descriptionIconApp: String = "App Icon",
+
+    // Credits
+    val makerContentCredits: String = "Gemaakt door Nathan Misset student HBO-ICT Game Development aan de Hogeschool van Amsterdam in 2024",
+    val opdrachtContentCredits: String = "In opdracht van het Lectoraat Legal Management van de Hogeschool van Amsterdam",
+    val ideeContentCredits: String = "Idee gebaseerd op Dielemma game van de Irasmus Universiteit en het Beterbezwaarspel van het lectoraat legal Managment",
+    val designContentCredits: String = "Gebaseerd op het design van Raquel de Romas student Communicatie en Multimedia Design aan de Hogeschool van Amsterdam",
+
+    )
+
+val LocalString= compositionLocalOf { com.example.intervision.ui.String() }
+
+val UiString: com.example.intervision.ui.String
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalString.current
--- app/src/main/java/com/example/intervision/ui/Theme.kt
+++ app/src/main/java/com/example/intervision/ui/Theme.kt
@@ -3,12 +3,10 @@ package com.example.intervision.ui
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-//import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -375,7 +373,7 @@ fun MyApplicationTheme(
         }
     }
 
-    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+    CompositionLocalProvider(LocalSpacing provides Spacing(), LocalString provides String()) {
         MaterialTheme(
             colorScheme = colorScheme,
             typography = Typography,
