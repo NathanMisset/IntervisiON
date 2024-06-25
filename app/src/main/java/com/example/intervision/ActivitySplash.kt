31a036e8d9e55a503b111fd044976693a0055a89
Nathan <nathan_misset@live.nl>
2024-06-25 15:39:05 +0200

    13/03/2024 ActivitySplash.kt Code style check

--- app/src/main/java/com/example/intervision/ActivitySplash.kt
+++ app/src/main/java/com/example/intervision/ActivitySplash.kt
@@ -1,9 +1,22 @@
+/**
+ * Copyright Lectoraat Legal Management van de Hogeschool van Amsterdam
+ *
+ * Gemaakt door Nathan Misset 2024
+ */
+
 package com.example.intervision
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * This activity is a splash screen at the moment just shows at the start of the app
+ * Should be used in the future as a loading screen and inform user of the backend activity
+ *
+ */
 
 class ActivitySplash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
