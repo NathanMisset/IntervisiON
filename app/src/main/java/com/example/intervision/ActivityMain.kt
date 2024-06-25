d2b8bd3d6738317609818c863a0ff26686a3cdb0
Nathan <nathan_misset@live.nl>
2024-06-25 12:11:40 +0200

    13/03/2024 ActivityMain.kt Code style check

--- app/src/main/java/com/example/intervision/ActivityMain.kt
+++ app/src/main/java/com/example/intervision/ActivityMain.kt
@@ -1,9 +1,22 @@
+/**
+ * Copyright Lectoraat Legal Management van de Hogeschool van Amsterdam
+ *
+ * Gemaakt door Nathan Misset 2024
+ */
+
 package com.example.intervision
 
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * This activity is where the applications start
+ * useful to launch a specifike activity for debugging
+ *
+ */
 
 class ActivityMain : AppCompatActivity() {
     private var loginType = 2
@@ -20,34 +33,26 @@ class ActivityMain : AppCompatActivity() {
         }
     }
 
-
     private fun launchRegister() {
-        //Launch register activity
         val i = Intent(this, ActivityRegister::class.java)
         startActivity(i)
     }
-
     private fun launchTutorial() {
-        //Launch Tutorial activity
         val i = Intent(this, ActivityTutorial::class.java)
         startActivity(i)
     }
-
     private fun launchLogin() {
         val i = Intent(this, ActivityLogin::class.java)
         startActivity(i)
     }
-
     private fun launchMakeGroup() {
         val i = Intent(this, ActivityMakeGroup::class.java)
         startActivity(i)
     }
-
     private fun launchHome() {
         val i = Intent(this, ActivityNavigation::class.java)
         startActivity(i)
     }
-
     private fun launchFragmentTest() {
         val i = Intent(this, ActivityNavigation::class.java)
         startActivity(i)
