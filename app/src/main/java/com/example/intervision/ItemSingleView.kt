8bb1e1e321a607a599e86e5f88f3d8f9da2130a3
Nathan <nathan_misset@live.nl>
2024-06-28 13:39:47 +0200

    13/03/2024 ItemSingleView.kt Code style check

--- app/src/main/java/com/example/intervision/ItemSingleView.kt
+++ app/src/main/java/com/example/intervision/ItemSingleView.kt
@@ -1,8 +1,21 @@
+/**
+ * Copyright Lectoraat Legal Management van de Hogeschool van Amsterdam
+ *
+ * Gemaakt door Nathan Misset 2024
+ */
+
 package com.example.intervision
 
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * This item can be initiated as an object in an activity
+ * This shows and image
+ *
+ */
 
 class ItemSingleView(imageID: Int, layout: View?) : AppCompatActivity() {
     init {
